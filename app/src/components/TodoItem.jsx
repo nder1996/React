ff15491233f25e79a9c02a34b7b19e6a0ac0f9a5
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2022-02-11 22:05:50 -0500

    Proyecto de prueba

---
+++ app/src/components/TodoItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/style.css'
+
+export default function TodoItem(){
+    return(
+        <>
+            <h1>
+               <strong>Cachon</strong>
+            </h1>
+        </>
+    );
+}
--- app/src/components/TodoList.jsx
+++ app/src/components/TodoList.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import prueba from "../prueba.jpg"
+import TodoItem from "./TodoItem";
+import {Button} from '@mui/material';
+
+
+
+
 
 function TodoList(){
     return(
         <>
         <h1>hola mundo</h1>
         <img src={prueba} alt="" />
-
+        <TodoItem />
+        <Button>Primary</Button>
+<Button disabled>Disabled</Button>
+<Button href="#text-buttons">Link</Button>
         </>
-     
     );
 }
 
