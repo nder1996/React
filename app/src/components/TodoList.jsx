9f14072a5fbb8d5402a245620cb10b4e46f35775
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2022-02-11 21:26:28 -0500

    Se ven imagenes

--- app/src/components/TodoList.jsx
+++ app/src/components/TodoList.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import prueba from "../prueba.jpg"
 
 function TodoList(){
     return(
-       <h1>hola mundo</h1>
+        <>
+        <h1>hola mundo</h1>
+        <img src={prueba} alt="" />
+
+        </>
+     
     );
 }
 
